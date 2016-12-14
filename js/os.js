e01bb5697e22c6deb18fd84612fa8542683fd2ba
zexuan <syltaris@gmail.com>
2016-12-14 11:08:56 +0800

    added library files and links

---
+++ js/os.js
@@ -0,0 +1,24 @@
+/**
+ * Created by psy on 2016/2/3.
+ */
+var os = function () {
+    var ua = navigator.userAgent,
+        isWindowsPhone = /(?:Windows Phone)/.test(ua),
+        isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+        isAndroid = /(?:Android)/.test(ua),
+        isFireFox = /(?:Firefox)/.test(ua),
+        isChrome = /(?:Chrome|CriOS)/.test(ua),
+        isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isPhone && !isAndroid && !isSymbian;
+    return {
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc,
+        isMobile: isAndroid || isPhone || isWindowsPhone || isSymbian
+    };
+}();
+if (os.isMobile) {
+    window.location.href = "http://gowild.cn/mobile";
+}
